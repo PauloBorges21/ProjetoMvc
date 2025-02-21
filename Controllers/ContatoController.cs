b39026a2174db7232b846b7283f9e5be410f9da1
paulo.borges <paulo.borges@orsoftware.com.br>
2025-02-21 18:47:24 -0300

    Criação de View Contato

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoMvc.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public ContatoController()
+        {
+
+        }
+
+        public IActionResult Index(){
+            return View();
+        }
+    }
+}
