4b670dc0788012cd7e0af636673e17745a8b44b1
paulo.borges <paulo.borges@orsoftware.com.br>
2025-02-24 17:17:51 -0300

    Remove o arquivo appsettings.Development.json, que contém strings de conexão e configurações de log específicas para o ambiente de desenvolvimento.

--- obj/Debug/net9.0/ProjetoMvc.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjetoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+123a80376b44d513e66bad3b7fce14ba1c93586e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+225a8632d891ea8682b3c676dcda5bfd97cddb78")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
